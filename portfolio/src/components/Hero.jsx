939ddeb6a940c4ac5a246fea5e5df998de51f79a
RosaliaNjoki <njosali0@gmail.com>
2025-01-05 08:59:52 +0800

    added about page

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -4,11 +4,11 @@ const Hero = ()=> {
     return (
         <section id='home'>
             <h1>Hello, I am Rosalia Njoki wanjohi</h1>
-            <p>I am a full-stack developer specializing in creating seamless and interactive web applications. My frontend expertise includes React, JavaScript, CSS, and HTML, where I focus on delivering responsive and user-friendly interfaces. On the backend, I leverage Python and Flask to build robust, scalable APIs and server-side applications. Additionally, I am skilled in database management, working proficiently with MySQL, PostgreSQL, and SQLite to ensure secure and efficient data handling. </p> 
-            <p>With a strong commitment to quality and innovation, I strive to develop solutions that combine performance, functionality, and aesthetics, driving impactful digital experiences.</p>
+            <p> Full-Stack Developer passionate about building impactful solutions.</p>
             <a href="#projects">View My Work</a>
 
         </section>
 
     );
 }; 
+export default Hero;
